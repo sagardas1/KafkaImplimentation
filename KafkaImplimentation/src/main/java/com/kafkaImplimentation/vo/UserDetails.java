4806dca627587f22d3e6f226c6a8135a75226082
sagardas1 <sagardas.195@gmail.com>
2020-09-03 12:22:53 +0530

    changes

---
+++ KafkaImplimentation/src/main/java/com/kafkaImplimentation/vo/UserDetails.java
@@ -0,0 +1,21 @@
+package com.kafkaImplimentation.vo;
+
+public class UserDetails {
+
+	
+	private String name;
+	private String redgNo;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRedgNo() {
+		return redgNo;
+	}
+	public void setRedgNo(String redgNo) {
+		this.redgNo = redgNo;
+	}
+	
+}
