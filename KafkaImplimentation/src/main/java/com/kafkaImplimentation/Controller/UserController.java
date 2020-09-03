8fbf25709555cc8f04c0a782a1ea05b15abce231
sagardas1 <sagardas.195@gmail.com>
2020-09-03 12:23:37 +0530

    changes

--- KafkaImplimentation/src/main/java/com/kafkaImplimentation/Controller/UserController.java
+++ KafkaImplimentation/src/main/java/com/kafkaImplimentation/Controller/UserController.java
@@ -1,7 +1,6 @@
 package com.kafkaImplimentation.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
