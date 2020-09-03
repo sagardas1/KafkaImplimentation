b4299c80010a25f3a55d6c023779c66f738ad486
sagardas1 <sagardas.195@gmail.com>
2020-09-03 12:23:17 +0530

    changes

--- KafkaImplimentation/src/main/java/com/kafkaImplimentation/config/KafkaConfig.java
+++ KafkaImplimentation/src/main/java/com/kafkaImplimentation/config/KafkaConfig.java
@@ -1,14 +1,31 @@
 package com.kafkaImplimentation.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import com.kafkaImplimentation.vo.UserDetails;
 
 @Configuration
 public class KafkaConfig {
+
+	@Bean
+	public ProducerFactory<String, UserDetails> producerFactory() {
+		Map<String, Object> config = new HashMap<String, Object>();
+
+		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.01:9092");
+		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, String.class);
+		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+		return new DefaultKafkaProducerFactory<String, UserDetails>(config);
+	}
 	
 	
-	
-	KafkaTemplate<String, String> getKafkatemplet;
 
 }
