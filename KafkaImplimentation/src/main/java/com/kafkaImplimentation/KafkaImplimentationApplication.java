1fd6707410bc1a2efabe5c006fd15b1d82d1eb8c
sagardas1 <sagardas.195@gmail.com>
2020-10-02 22:39:03 +0530

    changes

--- KafkaImplimentation/src/main/java/com/kafkaImplimentation/KafkaImplimentationApplication.java
+++ KafkaImplimentation/src/main/java/com/kafkaImplimentation/KafkaImplimentationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KafkaImplimentationApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(KafkaImplimentationApplication.class, args);
 	}
 
